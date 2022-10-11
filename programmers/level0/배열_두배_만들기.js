5785efae0d090094b5b7bf35084b1dad6a34f49c
jihyunglee <jhlee7125@gmail.com>
2022-10-11 23:14:22 +0900

    2022-10-11

--- programmers/level0/배열_두배_만들기.js
+++ programmers/level0/배열_두배_만들기.js
@@ -1,5 +1,13 @@
+// ed. 2022-10-11
+// 스프레드 문법 활용
 function solution(numbers) {
+  var answer = [...numbers].map((number) => number * 2);
+  return answer;
+}
+
+// push 메서드 사용
+/* function solution(numbers) {
   var answer = [];
   numbers.forEach((number) => answer.push(number * 2));
   return answer;
-}
+} */
--- programmers/level0/자릿수_더하기.js
+++ programmers/level0/자릿수_더하기.js
@@ -1,8 +1,20 @@
+// ed. 2022-10-11
 function solution(n) {
+  n += ''; // n = String(n);
+  var answer = n
+    .split('')
+    .map((v) => (v = parseInt(v)))
+    .reduce((sum, cur) => sum + cur, 0);
+  return answer;
+}
+
+/* function solution(n) {
   var answer = 0;
   n = String(n);
   for (let i = 0; i < n.length; i++) {
     answer += parseInt(n[i]);
   }
   return answer;
-}
+} */
+
+console.log(solution(1234));
