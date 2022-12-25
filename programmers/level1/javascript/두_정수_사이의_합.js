594763ea1c5341a338dde8693c81930440095f60
jihyunglee <jhlee7125@gmail.com>
2022-12-25 22:46:47 +0900

    2022-12-25

--- programmers/level1/javascript/두_정수_사이의_합.js
+++ programmers/level1/javascript/두_정수_사이의_합.js
@@ -1,4 +1,24 @@
 function solution(a, b) {
-  var answer = 0;
-  return answer;
+  return ((a + b) * (Math.abs(b - a) + 1)) / 2;
 }
+
+// my solution
+/**
+ * function solution(a, b) {
+    let result = 0;
+    let arr = [];
+    if (b < a) {
+        for (let i = b; i <= a; i++) {
+            arr.push(i);
+        }
+    } else {
+        for (let i = a; i <= b; i++) {
+            arr.push(i);
+        }
+    }
+    for (let j = 0; j < arr.length; j++) {
+        result += arr[j];
+    }
+    return result;
+}
+ */
