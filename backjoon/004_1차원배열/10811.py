154a4a07d146a43883f36bbb58f42fe34ee1babf
jihyunglee <jhlee7125@gmail.com>
2023-03-01 05:18:20 +0900

    바구니 뒤집기

---
+++ backjoon/004_1차원배열/10811.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+
+arr = []
+for b in range(N):
+    arr.append(str(b + 1))
+
+print(arr)
+t = 0
+while t < M:
+    i, j = map(int, input().split())
+    pick = arr[i - 1:j]
+    arr = arr[:i-1] + pick[::-1] + arr[j:]
+    t += 1
+
+print(' '.join(arr))
