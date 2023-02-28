ef3fd41ef62aea98b6f213d7cee63ca23b18912f
jihyunglee <jhlee7125@gmail.com>
2023-03-01 02:06:23 +0900

    공 넣기

---
+++ backjoon/004_1차원배열/10810.py
@@ -0,0 +1,22 @@
+# 1 3 1
+# 2 3 1 2
+# 3 4 1
+# 4 4 1
+# 5
+
+N, M = map(int, input().split())
+
+arr = []
+# 바구니 생성
+for b in range(N):
+    arr.append('0')
+
+t = 0
+while t < M:
+    i, j, k = map(int, input().split())
+    for idx in range(i, j + 1):
+        arr[idx - 1] = str(k)
+    
+    t += 1
+
+print(" ".join(arr))
