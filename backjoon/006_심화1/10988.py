0e20079de03cb03923025e52d3099737d93a1042
jihyunglee <jhlee7125@gmail.com>
2023-03-05 00:22:48 +0900

    팰린드롬인지 확인하기 - reversed() 이용

--- backjoon/006_심화1/10988.py
+++ backjoon/006_심화1/10988.py
@@ -1,12 +1,9 @@
 word = input()
 word_lst = list(word)
 
-reversed_lst = []
-
-for i in range(len(word_lst) - 1, -1, -1):
-    reversed_lst.append(word[i])
-
+reversed_lst = list(reversed(word_lst))
 reversed_word = ''.join(reversed_lst)
+
 if word == reversed_word:
     print(1)
 else:
