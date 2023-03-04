ee8ba3061d6c45d98b602c2bab3a42f37c637c0d
jihyunglee <jhlee7125@gmail.com>
2023-03-05 05:40:21 +0900

    최댓값

---
+++ backjoon/007_2차원배열/2566.py
@@ -0,0 +1,26 @@
+# 최댓값
+
+import math
+
+max_value = -math.inf
+row_count, row_index, col_index = 0, 0, 0
+
+while True:
+    try:
+        row_count += 1
+        row = list(map(int, input().split()))
+        max_in_row = max(row)
+
+        
+        if max_in_row > max_value:
+            max_value = max_in_row       
+            row_index = row_count
+            col_index = row.index(max_in_row) + 1
+            
+        print(max_in_row, max_value)
+
+    except:
+        break
+    
+print(max_value)
+print(row_index, col_index)
